87082ff25962dce9568b8fa8dc600bc9b14200e8
Liwei <liwei.jin97@gmail.com>
2021-02-04 16:09:06 +0800

    debug

--- configs/matcher/video_text_matcher/tsn_r50_1x1x8_100e_bert_2_sentence_usv.py
+++ configs/matcher/video_text_matcher/tsn_r50_1x1x8_100e_bert_2_sentence_usv.py
@@ -98,7 +98,7 @@ test_pipeline = [
     dict(type='ToTensor', keys=['imgs'])
 ]
 data = dict(
-    videos_per_gpu=10,
+    videos_per_gpu=16,
     workers_per_gpu=5,
     train=dict(
         type=dataset_type,
