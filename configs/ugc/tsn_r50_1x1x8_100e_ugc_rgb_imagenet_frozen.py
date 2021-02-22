42f2fff144d5c5c98893c3d2ccee964a014ae76e
Liwei <liwei.jin97@gmail.com>
2021-02-22 11:39:18 +0800

    fix

--- configs/ugc/tsn_r50_1x1x8_100e_ugc_rgb_imagenet_frozen.py
+++ configs/ugc/tsn_r50_1x1x8_100e_ugc_rgb_imagenet_frozen.py
@@ -117,7 +117,7 @@ log_config = dict(
 # runtime settings
 dist_params = dict(backend='nccl', port=29509)
 log_level = 'INFO'
-work_dir = './work_dirs/tsn_r50_1x1x8_100e_ugc_rgb/'
+work_dir = './work_dirs/tsn_r50_1x1x8_100e_ugc_rgb_imagenet_frozen/'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
