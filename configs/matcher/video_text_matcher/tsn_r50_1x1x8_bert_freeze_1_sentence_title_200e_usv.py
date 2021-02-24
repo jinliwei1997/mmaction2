347365b01a74752c28084e2c3a82dfef5227d5f1
Liwei <liwei.jin97@gmail.com>
2021-02-24 17:44:37 +0800

    debug

--- configs/matcher/video_text_matcher/tsn_r50_1x1x8_bert_freeze_1_sentence_title_200e_usv.py
+++ configs/matcher/video_text_matcher/tsn_r50_1x1x8_bert_freeze_1_sentence_title_200e_usv.py
@@ -139,7 +139,7 @@ log_config = dict(
     ])
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/usv_matcher_2021_2_24_3/'
+work_dir = './work_dirs/usv_matcher_2021_2_24_4/'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
