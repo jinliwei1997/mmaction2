d0a78a7b0b089f999614f75e1dfd764fcea942cc
Liwei <liwei.jin97@gmail.com>
2021-03-13 15:32:39 +0800

    save_v_backbone

--- tools/save_visual_backbone.py
+++ tools/save_visual_backbone.py
@@ -4,19 +4,20 @@ import torch
 def parse_args():
     parser = argparse.ArgumentParser(
         description='Process a checkpoint to be published')
-    parser.add_argument('in_file', help='input checkpoint filename')
-    parser.add_argument('out_file', help='output checkpoint filename')
+    parser.add_argument('--in_file', help='input checkpoint filename')
+    parser.add_argument('--out_file', help='output checkpoint filename')
+    parser.add_argument('--prefix', help='prefix to find')
     args = parser.parse_args()
     return args
 
-def process_checkpoint(in_file, out_file):
+def process_checkpoint(in_file, out_file, prefix):
     checkpoint = torch.load(in_file, map_location='cpu')['state_dict']
 
     backbone = {}
 
     for key in checkpoint:
-        if 'backbone1' in key:
-            backbone[key.replace('backbone1','backbone')] = checkpoint[key]
+        if prefix in key:
+            backbone[key.replace(prefix, 'backbone')] = checkpoint[key]
 
     torch.save(backbone, out_file)
 
@@ -24,7 +25,7 @@ def process_checkpoint(in_file, out_file):
 
 def main():
     args = parse_args()
-    process_checkpoint(args.in_file, args.out_file)
+    process_checkpoint(args.in_file, args.out_file, args.prefix)
 
 if __name__ == '__main__':
     main()
