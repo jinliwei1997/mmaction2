527d2d7a3b49ffc174d9b8f221a90a877e4c7423
Liwei <liwei.jin97@gmail.com>
2021-04-09 12:47:57 +0800

    debugf

--- configs/bili/v_t_e2e_200e_half_2_frame.py
+++ configs/bili/v_t_e2e_200e_half_2_frame.py
@@ -122,7 +122,7 @@ data = dict(
         pipeline=test_pipeline)
 )
 
-optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0001) # 4 gpus
+optimizer = dict(type='SGD', lr=0.05, momentum=0.9, weight_decay=0.0001) # 4 gpus
 optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
 lr_config = dict(policy='step', step=[50, 100, 150])
 total_epochs = 200
