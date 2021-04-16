a50a6a44f130ab59b71d038fc9664a2a5148da46
Liwei <liwei.jin97@gmail.com>
2021-04-16 11:19:24 +0800

    debug

--- pipeline_test.py
+++ pipeline_test.py
@@ -23,9 +23,9 @@ cfg = dict(
     ann_file = '/mnt/lustre/jinliwei/annotation/bili_video_dm_partial_test',
     data_prefix = '',
     pipeline = [
-        dict(type='OpenCVInit'),
+        dict(type='DecordInit'),
         dict(type='SampleFrames', clip_len=1, frame_interval=1, num_clips=8),
-        dict(type='OpenCVDecode'),
+        dict(type='DecordDecode'),
         dict(type='Resize', scale=(-1, 256), lazy=True),
         dict(
             type='MultiScaleCrop',
