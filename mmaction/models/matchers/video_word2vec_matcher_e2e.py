0f413cf014953c35d32450f9829db03fae654e7c
Liwei <liwei.jin97@gmail.com>
2021-04-21 18:42:38 +0800

    debug

--- mmaction/models/matchers/video_word2vec_matcher_e2e.py
+++ mmaction/models/matchers/video_word2vec_matcher_e2e.py
@@ -95,7 +95,7 @@ class VideoWord2VecMatcherE2E(nn.Module):
         # print(v_feat.shape)
         if self.neck is not None:
             v_feat, t_feat = self.neck(v_feat, t_feat)
-        print(v_feat.shape, t_feat.shape)
+        # print(v_feat.shape, t_feat.shape)
         return self.head(v_feat, t_feat)
 
     def forward_test(self, imgs, word2vec, weight):
@@ -162,6 +162,41 @@ class VideoWord2VecMatcherE2E(nn.Module):
     def val_step(self, data_batch, optimizer, **kwargs):
         pass
 
+    @staticmethod
+    def _parse_losses(losses):
+        """Parse the raw outputs (losses) of the network.
+
+        Args:
+            losses (dict): Raw output of the network, which usually contain
+                losses and other necessary information.
+
+        Returns:
+            tuple[Tensor, dict]: (loss, log_vars), loss is the loss tensor
+                which may be a weighted sum of all losses, log_vars contains
+                all the variables to be sent to the logger.
+        """
+        log_vars = OrderedDict()
+        for loss_name, loss_value in losses.items():
+            if isinstance(loss_value, torch.Tensor):
+                log_vars[loss_name] = loss_value.mean()
+            elif isinstance(loss_value, list):
+                log_vars[loss_name] = sum(_loss.mean() for _loss in loss_value)
+            else:
+                raise TypeError(
+                    f'{loss_name} is not a tensor or list of tensors')
+
+        loss = sum(_value for _key, _value in log_vars.items()
+                   if 'loss' in _key)
+
+        log_vars['loss'] = loss
+        for loss_name, loss_value in log_vars.items():
+            # reduce loss when distributed training
+            if dist.is_available() and dist.is_initialized():
+                loss_value = loss_value.data.clone()
+                dist.all_reduce(loss_value.div_(dist.get_world_size()))
+            log_vars[loss_name] = loss_value.item()
+
+        return loss, log_vars
 
 class GatherLayer(torch.autograd.Function):
     """Gather tensors from all process, supporting backward propagation.
