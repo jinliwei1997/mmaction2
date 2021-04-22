801027c639cf082cdc82c3a23effdf3301a1dfd2
Liwei <liwei.jin97@gmail.com>
2021-04-22 19:52:32 +0800

    debug

--- configs/bili/partial/v_t_e2e_50e_word2vec.py
+++ configs/bili/partial/v_t_e2e_50e_word2vec.py
@@ -111,7 +111,7 @@ data = dict(
         pipeline=test_pipeline)
 )
 
-optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.06, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
 lr_config = dict(
     policy='CosineAnnealing',
