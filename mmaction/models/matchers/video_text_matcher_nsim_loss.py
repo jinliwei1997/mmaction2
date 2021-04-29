774067f26ded2ba61c96f3a6b6d43dfa9f0ef9c9
Liwei <liwei.jin97@gmail.com>
2021-04-29 18:00:14 +0800

    nsim_eval

--- mmaction/models/matchers/video_text_matcher_nsim_loss.py
+++ mmaction/models/matchers/video_text_matcher_nsim_loss.py
@@ -110,19 +110,23 @@ class VideoTextMatcherNSimLoss(BaseMatcher):
         return self.head(v_feat, t_feat, p_v, p_t)
 
     def forward_test(self, imgs, texts_item):
-        pass
-        # N = imgs.shape[0]
-        # imgs = imgs.reshape((-1,) + imgs.shape[2:])
-        # v_feat = nn.functional.normalize(self.encoder_v(imgs, N), dim=1)  # [N , C]
-        # for key in texts_item:
-        #     texts_item[key] = texts_item[key].reshape((-1,) + texts_item[key].shape[2:])
-        # t_feat = nn.functional.normalize(self.encoder_t(texts_item), dim=1)  # [N * text_num_per_video (T), C]
-        # t_feat = t_feat.view(N, -1)  # [N , T * C]
-        #
-        # if self.neck is not None:
-        #     v_feat, t_feat = self.neck(v_feat, t_feat)
-        #
-        # return zip(v_feat.cpu().numpy(),t_feat.view(N, -1, v_feat.shape[1]).cpu().numpy())
+        N = imgs.shape[0]
+        imgs = imgs.reshape((-1,) + imgs.shape[2:])
+        v_feat = self.encoder_v(imgs, N)  # [N , C]
+        for key in texts_item:
+            texts_item[key] = texts_item[key].reshape((-1,) + texts_item[key].shape[2:])
+        t_feat = self.encoder_t(texts_item) # [N * text_num_per_video (T), C]
+
+        if self.gather_flag == True:
+            v_feat = torch.cat(GatherLayer.apply(v_feat), dim=0)
+            t_feat = torch.cat(GatherLayer.apply(t_feat), dim=0)
+
+        if self.neck is not None:
+            v_feat, t_feat = self.neck(v_feat, t_feat)
+
+        p_v = self.predictor_v(v_feat)
+        p_t = self.predictor_t(t_feat)
+        return zip(v_feat.cpu().numpy(),p_t.view(N, -1, v_feat.shape[1]).cpu().numpy())
 
     def forward_gradcam(self, audios):
         raise NotImplementedError
