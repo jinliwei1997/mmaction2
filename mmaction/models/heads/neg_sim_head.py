f182718ed4cb775a87b1a0ace5ddc2df0eaf8243
Liwei <liwei.jin97@gmail.com>
2021-04-29 13:54:48 +0800

    neg_sim_test

--- mmaction/models/heads/neg_sim_head.py
+++ mmaction/models/heads/neg_sim_head.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 import torch
 import torch.distributed as dist
-
+import torch.nn.functional as F
 from ..registry import HEADS
 
 @HEADS.register_module
@@ -9,72 +9,41 @@ class NegSimHead(nn.Module):
     """Head for RankingLoss.
 
     """
-
-    def __init__(self,
-        ):
+    def __init__(self):
         super(NegSimHead, self).__init__()
-
 
     def init_weights(self):
         """Initiate the parameters from scratch."""
         pass
 
-    def forward(self, v_feat, t_feat):
+    def forward(self, v_feat, t_feat, p_v, p_t):
         """Forward head.
 
         Args:
-            v_feat (Tensor): [N * 256]
-            t_feat (Tensor): [N * text_num_per_video(T), 256]
+            v_feat (Tensor): [N , C]
+            t_feat (Tensor): [N , C]
+            p_v (Tensor): [N , C]
+            p_t (Tensor): [N , C]
         Returns:
             dict[str, Tensor]: A dictionary of loss components.
         """
+        v_feat = v_feat.detach()
+        t_feat = t_feat.detach()
+        v_feat = F.normalize(v_feat, dim=1)
+        t_feat = F.normalize(t_feat, dim=1)
+        p_v = F.normalize(p_v, dim=1)
+        p_t = F.normalize(p_t, dim=1)
 
-        s = torch.matmul(v_feat, t_feat.permute(1, 0)) # [N , N * T]
-        s = s.view(v_feat.shape[0], v_feat.shape[0]) # [N , N]
-        N = s.shape[0]
-        # Neg Similarity loss
-        x = []
-        for i in range(N):
-            for j in range(N):
-                if j != i:
-                    x.append(i)
-
-        pos = s[torch.tensor(x), torch.tensor(x)]
         losses = dict()
-        losses['neg_sim_loss'] = -torch.mean(pos)
+        losses['neg_sim_loss'] = -0.5*(p_v * t_feat).sum(dim=1).mean() -0.5*(p_t * v_feat).sum(dim=1).mean()
 
-        s = s.view(v_feat.shape[0], v_feat.shape[0], -1)  # [N , N , T]
+
         with torch.no_grad():
-            N = s.shape[0]
-            T = s.shape[2]
-            s = s.view(v_feat.shape[0], -1)  # [N , N * T]
 
-            _, rank = torch.sort(s, dim=1, descending=True)
-
-            recall1 = torch.zeros(N).cuda()
-            recall5 = torch.zeros(N).cuda()
-            recall10 = torch.zeros(N).cuda()
-            mean_rk = torch.zeros(N).cuda()
-            for i in range(N):
-                for j in range(N * T):
-                    if rank[i][j].item() >= T * i and rank[i][j].item() < T * (i + 1):
-                        mean_rk[i] += j
-                        if j < 10:
-                            recall10[i] += 1
-                        if j < 5:
-                            recall5[i] += 1
-                        if j < 1:
-                            recall1[i] += 1
-
-            recall1 = torch.true_divide(recall1, T)
-            recall5 = torch.true_divide(recall5, T)
-            recall10 = torch.true_divide(recall10, T)
-            mean_rk = torch.true_divide(mean_rk, T)
-
-            metric = dict()
-            metric['recall1'] = torch.mean(recall1)
-            metric['recall5'] = torch.mean(recall5)
-            metric['recall10'] = torch.mean(recall10)
-            metric['mean_rk'] = torch.mean(mean_rk)
+            metric = {}
+            metric['v_feat_std'] = torch.mean(torch.std(v_feat, dim = 0))
+            metric['t_feat_std'] = torch.mean(torch.std(t_feat, dim = 0))
+            metric['p_v_std'] = torch.mean(torch.std(p_v, dim = 0))
+            metric['p_t_std'] = torch.mean(torch.std(p_t, dim = 0))
 
         return losses, metric
--- mmaction/models/matchers/__init__.py
+++ mmaction/models/matchers/__init__.py
@@ -5,5 +5,7 @@ from .video_audio_text_matcher_e2e import VideoAudioTextMatcherE2E
 from .video_subtitle_text_matcher_e2e import VideoSubtitleTextMatcherE2E
 from .video_subtitle_audio_text_matcher_e2e import VideoSubtitleAudioTextMatcherE2E
 from .video_word2vec_matcher_e2e import VideoWord2VecMatcherE2E
+from .video_text_matcher_nsim_loss import VideoTextMatcherNSimLoss
 __all__ = ['BaseMatcher', 'VideoTextMatcher','VideoTextMatcherE2E','VideoAudioTextMatcherE2E',
-           'VideoSubtitleTextMatcherE2E','VideoSubtitleAudioTextMatcherE2E','VideoWord2VecMatcherE2E']
+           'VideoSubtitleTextMatcherE2E','VideoSubtitleAudioTextMatcherE2E','VideoWord2VecMatcherE2E',
+           'VideoTextMatcherNSimLoss']
