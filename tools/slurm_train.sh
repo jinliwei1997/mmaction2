4601335a878ff8b28b0b24e120913151604639d3
Liwei <liwei.jin97@gmail.com>
2021-05-02 12:15:42 +0800

    config

--- tools/slurm_train.sh
+++ tools/slurm_train.sh
@@ -7,7 +7,7 @@ JOB_NAME=$2
 CONFIG=$3
 GPUS=${GPUS:-8}
 GPUS_PER_NODE=${GPUS_PER_NODE:-8}
-CPUS_PER_TASK=${CPUS_PER_TASK:-5}
+CPUS_PER_TASK=${CPUS_PER_TASK:-10}
 SRUN_ARGS=${SRUN_ARGS:-""}
 PY_ARGS=${@:4}  # Any arguments from the forth one are captured by this
 
