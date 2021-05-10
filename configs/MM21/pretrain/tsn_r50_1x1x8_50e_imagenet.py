b9f944b7003d6d6c5ff8f85dc2ca82c299fc5b38
Liwei <liwei.jin97@gmail.com>
2021-05-10 15:57:35 +0800

    adjust_lr

--- configs/MM21/pretrain/tsn_r50_1x1x8_50e_imagenet.py
+++ configs/MM21/pretrain/tsn_r50_1x1x8_50e_imagenet.py
@@ -107,8 +107,8 @@ data = dict(
         pipeline=test_pipeline))
 # optimizer
 optimizer = dict(
-    type='SGD', lr=0.1, momentum=0.9,
-    weight_decay=0.0005)  # this lr is used for 8*2 gpus
+    type='SGD', lr=0.02, momentum=0.9,
+    weight_decay=0.0001)  # this lr is used for 8*2 gpus
 optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
 # learning policy
 lr_config = dict(
