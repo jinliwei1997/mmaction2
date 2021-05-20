73c796b51273420e566aab5faf8f3ff5dfecf153
Liwei <liwei.jin97@gmail.com>
2021-05-20 15:13:22 +0800

    config_update

--- configs/MM21/pretrain/v_t_e2e_100e_bert_imagenet.py
+++ configs/MM21/pretrain/v_t_e2e_100e_bert_imagenet.py
@@ -120,7 +120,7 @@ data = dict(
         pipeline=test_pipeline)
 )
 
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)# this lr is used for 8 gpus
 optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
 lr_config = dict(
     policy='CosineAnnealing',
