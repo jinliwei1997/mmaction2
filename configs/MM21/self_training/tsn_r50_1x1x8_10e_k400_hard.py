081072ef5066a9a301be6be98edce61f409b87d3
Liwei <liwei.jin97@gmail.com>
2021-05-27 20:32:12 +0800

    config

--- configs/MM21/self_training/tsn_r50_1x1x8_10e_k400_hard.py
+++ configs/MM21/self_training/tsn_r50_1x1x8_10e_k400_hard.py
@@ -110,8 +110,8 @@ data = dict(
         pipeline=test_pipeline))
 # optimizer
 optimizer = dict(
-    type='SGD', lr=0.0125, momentum=0.9,
-    weight_decay=0.0005)  # this lr is used for 4 gpus
+    type='SGD', lr=0.005, momentum=0.9,
+    weight_decay=0.0005)  # this lr is used for 2 gpus
 optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
 # learning policy
 lr_config = dict(
