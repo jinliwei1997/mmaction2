c119703ffc03c0489c26205762a3dc88dfb7e636
Liwei <liwei.jin97@gmail.com>
2021-06-24 11:38:56 +0800

    video_dataset

--- mmaction/datasets/video_dataset.py
+++ mmaction/datasets/video_dataset.py
@@ -1,6 +1,8 @@
+import copy
 import os.path as osp
-
 import torch
+import numpy as np
+from collections import defaultdict
 
 from .base import BaseDataset
 from .registry import DATASETS
@@ -38,15 +40,22 @@ class VideoDataset(BaseDataset):
     """
 
     def __init__(self, ann_file, pipeline, start_index=0, **kwargs):
+        if kwargs.get("sample_by_class", False):
+            self.prob_by_class = dict()
+            self.class_idx = defaultdict(list)
         super().__init__(ann_file, pipeline, start_index=start_index, **kwargs)
+        if not self.test_mode and self.sample_by_class:
+            for label in self.video_infos_by_class:
+                number = len(self.video_infos_by_class[label])
+                self.prob_by_class[label] = np.ones(number) / number
 
     def load_annotations(self):
         """Load annotation file to get video information."""
-        if self.ann_file.endswith('.json'):
+        if self.ann_file.endswith(".json"):
             return self.load_json_annotations()
 
         video_infos = []
-        with open(self.ann_file, 'r') as fin:
+        with open(self.ann_file, "r") as fin:
             for line in fin:
                 line_split = line.strip().split()
                 if self.multi_class:
@@ -61,7 +70,62 @@ class VideoDataset(BaseDataset):
                 if self.data_prefix is not None:
                     filename = osp.join(self.data_prefix, filename)
                 video_infos.append(
-                    dict(
-                        filename=filename,
-                        label=onehot if self.multi_class else label))
+                    dict(filename=filename, label=onehot if self.multi_class else label)
+                )
         return video_infos
+
+    def parse_by_class(self):
+        video_infos_by_class = defaultdict(list)
+        for i, item in enumerate(self.video_infos):
+            label = item["label"]
+            video_infos_by_class[label].append(item)
+            self.class_idx[label].append(i)
+        return video_infos_by_class
+
+    def prepare_train_frames(self, idx):
+        """Prepare the frames for training given the index."""
+        if self.sample_by_class:
+            # Then, the idx is the class index
+            samples = self.video_infos_by_class[idx]
+            idx2 = np.random.choice(len(samples), p=self.prob_by_class[idx])
+            self.prob_by_class[idx][idx2] = 0
+            if np.sum(self.prob_by_class[idx]) <= 0:
+                self.prob_by_class[idx] = np.ones(len(samples)) / len(samples)
+            else:
+                self.prob_by_class[idx] /= np.sum(self.prob_by_class[idx])
+            results = copy.deepcopy(samples[idx2])
+            idx = self.class_idx[idx][idx2]
+        else:
+            results = copy.deepcopy(self.video_infos[idx])
+        results["idx"] = idx
+        results["modality"] = self.modality
+        results["start_index"] = self.start_index
+
+        # prepare tensor in getitem
+        # If HVU, type(results['label']) is dict
+        if self.multi_class and isinstance(results["label"], list):
+            onehot = torch.zeros(self.num_classes)
+            onehot[results["label"]] = 1.0
+            results["label"] = onehot
+
+        return self.pipeline(results)
+
+    def prepare_test_frames(self, idx):
+        """Prepare the frames for testing given the index."""
+        if self.sample_by_class:
+            # Then, the idx is the class index
+            samples = self.video_infos_by_class[idx]
+            results = copy.deepcopy(np.random.choice(samples))
+        else:
+            results = copy.deepcopy(self.video_infos[idx])
+        results["modality"] = self.modality
+        results["start_index"] = self.start_index
+
+        # prepare tensor in getitem
+        # If HVU, type(results['label']) is dict
+        if self.multi_class and isinstance(results["label"], list):
+            onehot = torch.zeros(self.num_classes)
+            onehot[results["label"]] = 1.0
+            results["label"] = onehot
+
+        return self.pipeline(results)
